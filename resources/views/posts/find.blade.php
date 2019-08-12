30bb1dfcf9c1aaade4a5be2b440d577846bf85aa
Arjun C <arjun391c@gmail.com>
2019-08-12 13:43:11 +0530

    test4

--- resources/views/posts/find.blade.php
+++ resources/views/posts/find.blade.php
@@ -5,14 +5,63 @@
 @section('content')
 <main role="main" class="inner cover" style="padding:20px">
     <div class="container">
-
         <div class="py-5 text-center">
 
           <h2>Find your stuff</h2>
           {{-- <p class="lead">Lets know your details</p> --}}
         </div>
 
-        <div class="jumbotron">
+        <div class="row">
+
+                <div class="col-md-4 order-md-2 mb-4">
+                        <h4 class="d-flex justify-content-between align-items-center mb-3">
+                          <span class="text-muted">Your cart</span>
+                          <span class="badge badge-secondary badge-pill">3</span>
+                        </h4>
+                        <ul class="list-group mb-3">
+                          <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                              <h6 class="my-0">Product name</h6>
+                              <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$12</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                              <h6 class="my-0">Second product</h6>
+                              <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$8</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between lh-condensed">
+                            <div>
+                              <h6 class="my-0">Third item</h6>
+                              <small class="text-muted">Brief description</small>
+                            </div>
+                            <span class="text-muted">$5</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between bg-light">
+                            <div class="text-success">
+                              <h6 class="my-0">Promo code</h6>
+                              <small>EXAMPLECODE</small>
+                            </div>
+                            <span class="text-success">-$5</span>
+                          </li>
+                          <li class="list-group-item d-flex justify-content-between">
+                            <span>Total (USD)</span>
+                            <strong>$20</strong>
+                          </li>
+                        </ul>
+
+                        <form class="card p-2">
+                          <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Promo code">
+                            <div class="input-group-append">
+                              <button type="submit" class="btn btn-secondary">Redeem</button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
 
           {{-- <div class="col-md-8 order-md-1">
             <hr class="mb-4">
@@ -57,6 +106,7 @@
 
               {{ Form::close() }}
           </div> --}}
+          <div class="chat-body chat-scroll" id="chat-scroll">
           <table id="data-table-keytable" class="table table-striped table-bordered">
                 <thead>
                     <tr>
